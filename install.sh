966f5f3a1ea51f5723db3d2cb7a8f24141cb3a73
Fernando Daciuk <f.daciuk@gmail.com>
2016-05-27 17:03:55 -0300

    Fix install command

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 wget https://raw.githubusercontent.com/fdaciuk/avit-da2k/master/avit-da2k.zsh-theme
 mv ./avit-da2k.zsh-theme ~/.oh-my-zsh/themes/
-sed -i 's/ZSH_THEME=.*/ZSH_THEME="avit-da2k"/'
+sed -i 's/ZSH_THEME=.*/ZSH_THEME="avit-da2k"/' ~/.zshrc
 source ~/.zshrc
